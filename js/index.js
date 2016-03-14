af873713aa6fc1c01788dccff6297379966d4c1e
Kandice <kandice.levero@gmail.com>
2016-03-13 23:37:18 -0400

    Created About page and Contact popuo

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
-$('.letter_box').hover(
+/********** ALPHABET LETTER HOVER ************/
+
+$('.letter_box span').hover(
 	function() {
-    	$(this).find('.bg').animate({opacity: 1}, 800);
+    	$(this).parent().find('.bg').animate({opacity: 1}, 800);
     }, 
     function() {
-        $(this).find('.bg').animate({opacity: 0}, 0);
+        $(this).parent().find('.bg').animate({opacity: 0}, 0);
     }
 );
 
-$('.pink_letter_box').hover(
+$('.pink_letter_box span').hover(
 	function() {
-    	$(this).find('.pink_bg').animate({opacity: 1}, 800);
+    	$(this).parent().find('.pink_bg').animate({opacity: 1}, 800);
     }, 
     function() {
-        $(this).find('.pink_bg').animate({opacity: 0}, 0);
+        $(this).parent().find('.pink_bg').animate({opacity: 0}, 0);
     }
 );
 
@@ -73,10 +75,116 @@ $(window).scroll(
     }
 );
 
+/****** ABOUT PAGE CHANGES ******/
+var about = false;
+
+$(".about").click(function(){
+
+    toggleAbout();
+
+});
 
 
+$(".x_button").click(function(){
+    
+    toggleAbout();
+
+});
 
 
+function toggleAbout()
+{
+
+    if (about === false) {
+        $(".section_1").removeClass("main_image");
+        $(".section_1").addClass("about_image")
+            .stop()
+            .animate(
+                {'padding-top':'120px'},
+                1800
+            );
+
+       $('.x_button, .about_body, .about_p').delay(600).css({'display':'block', 'opacity':'1'});
+
+        $('.logo')
+            .css(
+                {'color':'white'}
+            )
+            .stop()
+            .animate(
+                {'padding-top':'180px'},
+                2000
+            );
+
+        $('.blue_line').css({'border-bottom':'1.5px solid white'});
+
+        about = true;
+
+        $(".us, .nav_ol, .intro_block, .bub_parent, .alphabet_grid, .g_block_img, .cocktail_intro_parent, .cocktail_body").hide();
+        
+
+    } else {
+        $(".section_1").addClass("main_image");
+        $(".section_1").removeClass("about_image").stop()
+            .animate(
+                {'padding-top':'0px'},
+                1800
+            );
+
+        $('.x_button, .about_body, .about_p').css({'display':'none', 'opacity':'0'});
+
+         $('.logo')
+            .css(
+                {'color':'#00004d'}
+            )
+            .stop()
+            .animate(
+                {'padding-top':'120px'},
+                2000
+            );
+
+        $('.blue_line').css({'border-bottom':'1.5px solid #00004d'}); 
+
+            $(".us, .nav_ol, .intro_block, .bub_parent, .alphabet_grid, .g_block_img, .cocktail_intro_parent, .cocktail_body").show();
+            
+
+        about = false;
+    }
+
+}
+
+/****** POP UP CONTACT ******/
+
+var contact = false;
+
+$(".contact").click(function(){
+
+    toggleContact();
+
+});
+
+
+$(".mail_button").click(function(){
+    
+    toggleContact();
+
+});
+
+function toggleContact()
+{   
+    /*console.log(contact)*/
+    if (contact === false) {
+
+        $('.contact_body').animate({'top':'300px'}, 600);
+        contact = true;
+
+    } else {
+
+         $('.contact_body').animate({'top':'-550px'}, 200);
+         contact = false;
+
+    }
+};
 
 
 
